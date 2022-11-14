9dc2980e99367ce44eb5f5b33ecb45e705034142
lucasomar84 <85948604+lucasomar84@users.noreply.github.com>
2022-11-14 19:24:47 -0300

    Update test.java

--- src/test/test.java
+++ src/test/test.java
@@ -1,68 +1,70 @@
-package test;
+package Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
-import calculadora.calculadora;
+import calculadoraa.calculadora;
 
-import java.util.Arrays;
+import java.lang.reflect.Array;
 
-import static org.junit.Assert.*;
+public class calculadoraTest {
 
-public class test {
     calculadora calculator = new calculadora();
 
     @Test
-    public void sumaTestOk() {
-        assertEquals(calculator.sumar(1, 1), 2);
+    public void sumaTest() {
+        assertEquals(calculator.sumar(1, 2), 3);
     }
 
     @Test
     public void sumaTestFail() {
-        assertNotEquals(calculator.sumar(1, 1), 5);
-    }
-
-    @Test
-    public void restarTestok() {
-        assertEquals(calculator.restar(320, 319), 1);
-    }
-
-    @Test
-    public void restarTestfail() {
-        assertNotEquals(calculator.restar(2, 1), 5);
-    }
-
-
-
-    @Test
-    public void multiplicarTestok() {
-        assertEquals(calculator.multiplicar(9, 2), 18);}
-
-    @Test
-    public void multiplicarTestfail() {
-        assertNotEquals(calculator.multiplicar(4, 3), 11);
+        assertNotEquals(calculator.sumar(4,1),3);
     }
 
 
     @Test
-    public void dividirTestok() {
-        assertEquals(calculator.dividir(10, 2), 5);
+    public void restarTest() {
+        assertEquals(calculator.restar(5, 2), 3);
     }
 
     @Test
-    public void dividirTestfail() {
-        assertNotEquals(calculator.dividir(11, 3), 5);
+    public void restarTestFail() {
+        assertNotEquals(calculator.restar(3,1),3);
     }
 
+    @Test
+    public void multiplicarTest() {
+        assertEquals(calculator.multiplicar(5, 2), 10);
+    }
+
+    @Test
+    public void multiplicarTestFail() {
+        assertNotEquals(calculator.multiplicar(4,1),3);
+    }
+
+    @Test
+    public void dividirTest() {
+        assertEquals(calculator.dividir(6, 2), 3);
+    }
+
+    @Test
+    public void dividirTestFail() {
+        assertNotEquals(calculator.dividir(6, 2), 4);
+        assertNotEquals(calculator.dividir(3, 2), 4);
+        try{assertNotEquals(calculator.dividir(6, 0), 3);}catch (ArithmeticException e){System.out.println(e);}
+    }
+    
     @Test(expected = ArithmeticException.class)
-    public void div2TestException() {
+    public void dividirExceptionTest(){
         calculator.dividir(1,0);
     }
 
+
     @Test
     public void raizCuadraticaTest(){
-        double[] result = calculator.raizCuadratica(4.0, 4.0, -3.0);
-        double [] Expectedresults  = {-0.5, 1.5};
-        assertFalse(Arrays.equals(Expectedresults, result));
+        double[] response;
+        assertEquals(calculator.raizCuadratica(1,2,-8),new double[] rta,new double[1]);
     }
-
 }
